f4dc05f2ebc04cad7ca5b42c59b99b3edeb07124
Jaysurieya <jaysurieyayuvaraja@gmail.com>
2025-08-03 11:53:26 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Hero } from './components/Hero';
 import { Route , Routes} from 'react-router-dom';
+import Stepper from './components/Stepper/Stepper';
 
 
 function App() {
   return(
     <Routes>
       <Route path='/' element={<Hero />} />
+      <Route path='/signup' element={<Stepper />} />
     </Routes>
   );
 }
