9be79f924e396e131bb5d5252dc7eeb1523b5fc2
Jaysurieya <jaysurieyayuvaraja@gmail.com>
2025-08-03 11:38:14 +0530

    routing initialised

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
 import './App.css';
-import { BackgroundBeams } from "./components/BackgroundBeams";
-import { TypewriterEffect } from "./components/TypewriterEffect";
-import AnimatedContent from "./components/AnimatedContent/AnimatedContent";
-import ShinyText from './components/ShinyText/ShinyText';
+import { Hero } from './components/Hero';
+import { Route , Routes} from 'react-router-dom';
+
 
 function App() {
   return(
-    <div className="app-container">
-      <div className="background-container">
-        <BackgroundBeams />
-      </div>
-
-      <div className="content-container">
-      <TypewriterEffect
-        words={[{ text: "Welcome to Fitmate" }]}
-        className="text-white text-3xl sm:text-4xl md:text-5xl font-semibold tracking-wide text-center"
-        cursorClassName="text-pink-500"
-      />
-        <div className='flex-row'>
-          <AnimatedContent delay={3} distance={150}>
-            <button className="buttons">
-              <ShinyText text='Get Started' />
-            </button>
-          </AnimatedContent>
-          <AnimatedContent delay={3} distance={150}>
-            <button className="buttons" >
-            <ShinyText text='Sign Up' />
-            </button>
-          </AnimatedContent>
-        <div>
-
-          </div> 
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<Hero />} />
+    </Routes>
   );
 }
 
